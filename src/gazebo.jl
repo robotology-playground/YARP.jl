c6e86879f59bd815cd9b8f6d5f29cfced865c26c
= <anothersms@gmail.com>
2016-05-16 15:38:43 +0200

    gazebo.jl: more helpers

--- src/gazebo.jl
+++ src/gazebo.jl
@@ -6,6 +6,45 @@ type Robot
     address::AbstractString
 end
 
+"Helper for text bottle creation"
+function newbottle(msg::AbstractString)
+    bottle = YARP.portableStruct()
+    YARP.bottleinit(bottle)
+    addstring(bottle, msg)
+    bottle
+end
+
+## Create prapared output bottles, for faster sending
+const bottlerack = Dict(
+ "step" => newbottle("stepSimulation"),
+ "stepAndWait" => newbottle("stepSimulationAndWait"),
+ "reset" => newbottle("resetSimulationTime"),
+ "pause" => newbottle("pauseSimulation")
+ ## FIXME: add missing
+ ## TODO: convert to macro?
+)
+
+"Helper for yarp opennewport and yarp connectnet"
+function connect(net,
+                 writername::AbstractString,
+                 outputport::AbstractString;
+                 contype = "shmem")
+    writer = opennewport(net, writername)
+    connectnet(net, writername, outputport, contype)
+    writer
+end
+
+function pause(writer)
+    input = YARP.portableStruct()
+    YARP.bottleinit(input)
+    output = YARP.portableStruct()
+    YARP.bottleinit(output)
+    addstring(output, "pauseSimulation")
+    YARP.writewreplyport(writer, output, input)
+    text = YARP.newstring()    
+    YARP.bottlestring(input, text)
+    YARP.stringtoc(text)
+end
 
 function reset(writer)
     input = YARP.portableStruct()
@@ -35,19 +74,21 @@ function simtime(writer)
     YARP.writewreplyport(writer, output, input)
     text = YARP.newstring()    
     YARP.bottlestring(input, text)
-    YARP.stringtoc(text)
+    parse(Float64, YARP.stringtoc(text))
 end
 
 function step(writer;blocking = false)
     input = YARP.portableStruct()
     YARP.bottleinit(input)
-    output = YARP.portableStruct()
-    YARP.bottleinit(output)
-    addstring(output, "stepSimulation")
+    if ! blocking
+        output = bottlerack["step"]
+    else
+        output = bottlerack["stepAndWait"]
+    end
     YARP.writewreplyport(writer, output, input)
     text = YARP.newstring()    
     YARP.bottlestring(input, text)
-    YARP.stringtoc(text) == "[done]" ? true : error("Could not step")
+    YARP.stringtoc(text) != "[fail]" ? true : error("Could not step")
 end
 
 ## ## Get motion done
@@ -57,9 +98,9 @@ end
 
 function angle(writer, jnt) ## TODO: check for multiple joint
     text = getenc(writer, jnt)
-    regex = r".*enc\s([0-9\.]+).*"
+    regex = r".*enc\s([0-9\.\-]+).*"
     if ismatch(regex, text)
-        match(regex, text)[1]
+        parse(Float64, match(regex, text)[1])
     else
         error("Read fail")
     end
@@ -86,6 +127,7 @@ function getenc(writer, jnt)
     text = YARP.newstring()
     input = YARP.portableStruct()
     YARP.bottleinit(input)
+    ## TODO: prepare output with a macro?
     output = YARP.portableStruct()
     YARP.bottleinit(output)
     addstring(output, "get")
